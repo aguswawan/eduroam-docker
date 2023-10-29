58a24cb45ab8ee3e3c804e223bbd7585eb586b95
agus setiawan <aguswawan93@gmail.com>
2023-10-29 21:03:22 +0700

    fixing uidnumber
    autoincrement uidnumber

--- csv_to_ldif.py
+++ csv_to_ldif.py
@@ -6,6 +6,7 @@ def csv_to_ldif(input_csv, output_ldif):
     
     # Buat file LDIF untuk menulis data
     with open(output_ldif, 'w') as ldif_file:
+        uidnumber = 1100
         # Iterasi melalui baris-baris dalam data CSV
         for index, row in data.iterrows():
             # Mendapatkan kolom-kolom dari CSV
@@ -14,6 +15,7 @@ def csv_to_ldif(input_csv, output_ldif):
             last_name = row['last_name']
             email = row['email']
             password = row['password']
+
             
             # Tulis data ke file LDIF
             ldif_file.write(f"dn: uid={username},ou=pegawai,dc=example,dc=org\n")
@@ -27,9 +29,10 @@ def csv_to_ldif(input_csv, output_ldif):
             ldif_file.write(f"objectclass: top\n")
             ldif_file.write(f"sn: {last_name}\n")
             ldif_file.write(f"uid: {username}\n")
-            ldif_file.write(f"uidnumber: 1004\n")
+            ldif_file.write(f"uidnumber: {uidnumber + 1}\n")
             ldif_file.write(f"userpassword: {password}\n")
             ldif_file.write("\n")
+            uidnumber += 1
 
     print(f"Konversi dari {input_csv} ke {output_ldif} selesai.")
 

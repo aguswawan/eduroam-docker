3ec70747928708661486d5672cf018caba4806e4
agus setiawan <aguswawan93@gmail.com>
2023-11-01 12:11:35 +0700

    fixing ldap config
    fixing ldap config

--- raddb/start.sh
+++ raddb/start.sh
@@ -3,10 +3,10 @@ EDUROAM_FLR="103.220.113.18"
 FLR_EDUROAM_SECRET="testing123"
 YOUR_REALM1="unand.ac.id"
 YOUR_REALM2="student.unand.ac.id"
-LDAP_SERVER="172.17.0.3"
-LDAP_IDENTITY="cn=admin,dc=example,dc=org"
+LDAP_SERVER="'172.17.0.3'"
+LDAP_IDENTITY="'cn=admin,dc=example,dc=org'"
 LDAP_PASSWORD="bismillah"
-LDAP_BASEDN="dc=example,dc=org"
+LDAP_BASEDN="'dc=example,dc=org'"
 
 chown -R freerad:freerad /etc/freeradius/3.0
 sed -i -e "s/EDUROAM_FLR/$EDUROAM_FLR/g" \
